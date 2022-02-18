a3a2f6158b62f35f0f85a116e2fe2519c89fd96d
= <rtyenriques@gmail.com>
2022-02-17 23:03:36 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
