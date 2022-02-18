a13a8722065a9307ba70f0504d8ea02561ad9933
= <rtyenriques@gmail.com>
2022-02-17 23:03:21 -0800

    commit tree 2e24f6fccd90414ba3718f1bedc1ec09cf7c9db2

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,52 @@
 // Code SimpleComponent Here
+import React from "react"
+
+class SimpleComponent extends React.Component {
+  state = {
+      mood: "happy"
+  }
+
+  handleClick = (e) => {
+//   debugger
+  if (this.state.mood === "happy"){
+      this.setState({
+          mood: "sad"
+      }) 
+  }else if(this.state.mood === "sad"){
+    this.setState({
+        mood: "happy"
+    })
+  }
+}
+// this.setState({
+//     mood: "sad"
+
+
+// handleClick(ev){
+    
+//     const { mood } = this.state;
+//     if (mood === 'happy'){
+//       this.setState({ mood: 'sad' });
+//     }else{
+//       this.setState({ mood: 'happy' });
+//     }
+//   }
+
+  render(){
+      return(
+          <div onClick={this.handleClick}>
+              {this.state.mood}
+              {/* <button 
+                className="happy"
+                onClick={this.handleClick}>
+                 
+              </button> */}
+          </div>
+      
+
+          
+      )
+  }
+}
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
+import React from "react"
 // Code SimplerComponent Here
+const SimplerComponent = ({ handleClick }) => {
+    
+    return(
+        <div onClick={handleClick}>
+            I am just happy
+        </div>
+    )
+
+}
+
+export default SimplerComponent
